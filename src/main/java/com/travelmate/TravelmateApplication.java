accee62a68c716198f76f7470d3eed20b1b81243
tnrms08 <wjddlfla661@naver.com>
2026-07-29 20:42:19 +0900

    Initial commit

---
+++ src/main/java/com/travelmate/TravelmateApplication.java
@@ -0,0 +1,13 @@
+package com.travelmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travelmate/TravelmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
